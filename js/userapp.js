b7e9541eb1778dd51fda06639bfb34fcf5a4077d
Samsuj <samsujj@gmail.com>
2016-06-02 13:39:22 +0530

    Initial commit

---
+++ js/userapp.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/* App Module */
+var r1headzappvaruser = angular.module('r1headzapp', ['ui.router','angularValidator','ngCookies','ui.bootstrap','ngFileUpload','ui.tinymce']);
+
+
+
+
+r1headzappvaruser.controller('user', function($scope,$state,$cookieStore,$rootScope,contentservice) {
+
+console.log(1);
+
+});
+
+r1headzappvaruser.controller('ModalInstanceCtrl', function ($scope, $uibModalInstance, items) {
+
+
+});
